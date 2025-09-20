ed5661071779542c7223d5d2890164d9df721ce8
suk-6 <me@suk.kr>
2025-09-20 17:10:49 +0900

    KoNLPy testing

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from konlpy.tag import Okt
+
+
 def main():
-    print("Hello from backend!")
+    okt = Okt()
+    text = "나는 학교에 간다. 학교가 끝나면 다시 집에 간다."
+    print("morphs:", okt.morphs(text))
+    print("nouns:", okt.nouns(text))
+    print("phrases:", okt.phrases(text))
+    print("pos:", okt.pos(text))
 
 
 if __name__ == "__main__":
